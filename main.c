60d65b702ca1b77d1d198bdefa24535d9cd4413d
PrajvalRaval <ravalprajval@gmail.com>
2023-12-22 23:10:13 +0530

    changing iptables

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int tun = openTun("tun0");
 	struct tcp_conn conn;
-	TCPConnection(tun, "192.0.3.2", 80, &conn);
+	TCPConnection(tun, "192.0.3.2", 1395, &conn);
 	char buffer[1024] = {0};
 
 	// Sending a SYN packet
--- setup.sh
+++ setup.sh
@@ -8,15 +8,18 @@ sudo ip tuntap add name tun1 mode tun user $USER
 sudo ip link set tun1 up
 sudo ip addr add 192.0.3.1 peer 192.0.3.2 dev tun1
 
-sudo iptables -t nat -A POSTROUTING -s 192.0.2.2 -j MASQUERADE
+# sudo iptables -t nat -A POSTROUTING -s 192.0.2.2 -j MASQUERADE
 sudo iptables -A FORWARD -i tun0 -s 192.0.2.2 -j ACCEPT
-sudo iptables -A FORWARD -o tun0 -d 192.0.2.2 -j ACCEPT
+sudo iptables -A FORWARD -o tun0 -d 192.0.3.2 -j ACCEPT
 
-sudo iptables -t nat -A POSTROUTING -s 192.0.3.2 -j MASQUERADE
+# sudo iptables -t nat -A POSTROUTING -s 192.0.3.2 -j MASQUERADE
 sudo iptables -A FORWARD -i tun1 -s 192.0.3.2 -j ACCEPT
 sudo iptables -A FORWARD -o tun1 -d 192.0.3.2 -j ACCEPT
 
-sudo iptables -A FORWARD -i tun0 -o tun1 -j ACCEPT
-sudo iptables -A FORWARD -i tun1 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# sudo iptables -A FORWARD -i tun0 -o tun1 -j ACCEPT
+# sudo iptables -A FORWARD -i tun1 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# sudo iptables -A INPUT -i tun1 -j ACCEPT
+# sudo iptables -A INPUT -i tun0 -j ACCEPT
 
 sudo sysctl -w net.ipv4.ip_forward=1
